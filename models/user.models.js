604eed61f03a07de486e458a3c5c302d79710a3c
hossam496 <albasuonyh@gmail.com>
2026-03-13 02:40:06 +0200

    Add role field to user model

--- models/user.models.js
+++ models/user.models.js
@@ -13,9 +13,13 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
     }
 })
 
 const userModel = mongoose.model('User', userSchema)
-
 export default userModel
